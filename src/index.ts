350948f7fccd10eae5997d1d9f2bb1d74a749979
Rexben001 <rexben.rb@gmail.com>
2019-11-03 23:01:19 +0100

    create a function to display the result

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { from } from "rxjs";
 
+import { map, catchError } from "rxjs/operators";
+
 const div = document.querySelector("div");
 
 const getDataFromURL = (input: string) =>
@@ -9,3 +11,20 @@ const getDataFromURL = (input: string) =>
 
 const streamFactory = (input: string) =>
   from(getDataFromURL(input)).pipe(map((value: any) => value));
+
+const displayResult = (arr: Array<string>) => {
+  for (let i = 0; i < arr.length; i++) {
+    const childDiv = document.createElement("div");
+    streamFactory(arr[i])
+      .pipe(catchError(err => err))
+      .subscribe((event: any) => {
+        event.location
+          ? (childDiv.innerHTML = `<p>Name: ${event.location.name}</p>
+              <p>Local Time: ${event.location.localtime.split(" ")[1]}</p>
+              <p>Temperature: ${event.current.temperature} &deg;F</p> <br>
+              `)
+          : (childDiv.innerHTML = `<p>Invalid location nameor postal code for ${arr[i]}</p> <br>`);
+        div.appendChild(childDiv);
+      });
+  }
+};
