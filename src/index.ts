7e7b36c2c42df50388fb53926a84a899376c9353
Rexben001 <rexben.rb@gmail.com>
2019-11-03 22:52:06 +0100

    fetch data from api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 const div = document.querySelector("div");
 
+const getDataFromURL = (input: string) =>
+  fetch(
+    `http://api.weatherstack.com/current?access_key=eacaca2dfa36519a9971955469b53ba1&query=${input}`
+  ).then(res => res.json());
