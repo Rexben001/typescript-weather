94f2326b32e9a37a70f69a60499cb0c909953464
Rexben001 <rexben.rb@gmail.com>
2019-11-03 22:56:39 +0100

    turn promise into an observable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { from } from "rxjs";
+
 const div = document.querySelector("div");
 
 const getDataFromURL = (input: string) =>
   fetch(
     `http://api.weatherstack.com/current?access_key=eacaca2dfa36519a9971955469b53ba1&query=${input}`
   ).then(res => res.json());
+
+const streamFactory = (input: string) =>
+  from(getDataFromURL(input)).pipe(map((value: any) => value));
